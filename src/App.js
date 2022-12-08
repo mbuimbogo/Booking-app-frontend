2a8ce63812127443b1855674d815ece45ecebde1
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-12-08 23:38:51 +0300

    Control BookingForm

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import './App.css';
 import {Routes, Route } from'react-router-dom';
 import { useEffect, useState } from 'react';
 import Navbar from './Components/Navbar';
-// import Home from './Components/Home';
+import BookingForm from './Components/BookingForm';
 import ReviewPages from './Components/ReviewPage';
 import UserList from './Components/UserList';
 import  Login from './pages/Login';
 
-
-
-
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState("");
+ 
+  const [formData, setFormData] = useState("");
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch("https://restaurant-booking-app-production.up.railway.app/bookings", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => setFormData(data));
+  }
+ 
+  function handleChange(event) {
+    const key = event.target.name;
+    setFormData({
+      ...formData,
+      [key]: event.target.value,
+    })
+  }
 
   useEffect(() => {
     // auto-login
@@ -29,8 +50,12 @@ function App() {
       <Navbar user={user} setUser={setUser} />
       <main>
         <Routes>
-          
-          {/* <Route path='/' element={<Home/>}/> */}
+      <Route path='/' element={<BookingForm
+      
+      formData = {formData}
+      handleChange={handleChange}
+        handleSubmit = {handleSubmit}
+      />}/>
       <Route exact path='/users' element={<UserList/>}/>
       <Route exact path='/reviews' element={<ReviewPages/>}/>
         </Routes>
